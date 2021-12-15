0ab1607f7631e67f6cad0b172fb278c3fe5c9df2
satyam malhotra <satyammalhotra@satyams-MacBook-Air.local>
2021-12-15 17:28:01 +0530

    First commit

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+
+from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+def post_list(request):
+    all_posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    print(all_posts)
+    return render(request,'blog/post_list.html',{'all_posts':all_posts})
